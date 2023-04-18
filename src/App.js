ab2e0f3136fdabb91aef832085e243f7da6a212e
AshG-1 <ashray.gupta2002@gmail.com>
2023-04-18 23:39:24 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import JobListings from './components/JobListings';
 import JobDetails from './components/JobDetails';
 import ApplyForm from './components/ApplyForm';
 import PostForm from './components/PostForm';
+import JobBoard from './components/JobBoard';
+
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import axios from 'axios';
 import './App.css';
 
 function App() {
   const [jobListings, setJobListings] = useState([]);
-
+  <div>
+    <h1>Job Board App</h1>
+    <JobBoard />
+  </div>
   useEffect(() => {
     axios.get('https://your-api.com/jobs')
       .then(res => setJobListings(res.data))
